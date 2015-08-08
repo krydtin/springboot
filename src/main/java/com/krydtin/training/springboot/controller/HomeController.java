a84a36ddc31f43fb1e2b68feb703dbde67f541b1
krydtin <localhost.max@gmail.com>
2015-08-08 15:01:34 +0700

    add input form

--- src/main/java/com/krydtin/training/springboot/controller/HomeController.java
+++ src/main/java/com/krydtin/training/springboot/controller/HomeController.java
@@ -3,6 +3,7 @@ package com.krydtin.training.springboot.controller;
 import com.krydtin.training.springboot.dto.Comment;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -21,7 +22,15 @@ public class HomeController {
         comment.setComment("Test Comment");
 
         model.addAttribute("comment", comment);
+        model.addAttribute("formComment", new Comment());
         return "index";
     }
-
+    
+    @RequestMapping(method = RequestMethod.POST)
+    public String save(@ModelAttribute("formComment") final Comment comment, Model model) {
+        System.out.println(comment.getComment());
+        model.addAttribute("comment", comment);
+        model.addAttribute("formComment", new Comment());
+        return "index";
+    }
 }
