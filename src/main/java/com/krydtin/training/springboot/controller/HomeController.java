cfcc938b08f52122a7c5aaba5684a380592fae55
krydtin <localhost.max@gmail.com>
2015-08-08 14:09:13 +0700

    add package dto -- Comment Modify HomeController -- add parameter Model

--- src/main/java/com/krydtin/training/springboot/controller/HomeController.java
+++ src/main/java/com/krydtin/training/springboot/controller/HomeController.java
@@ -1,6 +1,8 @@
 package com.krydtin.training.springboot.controller;
 
+import com.krydtin.training.springboot.dto.Comment;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -13,8 +15,13 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class HomeController {
 
     @RequestMapping(method = RequestMethod.GET)
-    public String home() {
+    public String home(Model model) {
+        final Comment comment = new Comment();
+        comment.setAuthor("John");
+        comment.setComment("Test Comment");
+
+        model.addAttribute("comment", comment);
         return "index";
     }
-    
+
 }
---
+++ src/main/java/com/krydtin/training/springboot/dto/Comment.java
@@ -0,0 +1,15 @@
+package com.krydtin.training.springboot.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ *
+ * @author krydtin
+ */
+@Getter
+@Setter
+public class Comment {
+    private String comment;
+    private String author;
+}
